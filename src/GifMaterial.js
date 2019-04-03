925a6d23a6cd83f97bbae96f29eb45614cda5da7
amitrajput1992 <amitrajput123.ar@gmail.com>
2019-04-03 19:55:00 +0530

    WIP: commit

--- src/GifMaterial.js
+++ src/GifMaterial.js
@@ -1,7 +1,210 @@
+/**
+ * @format
+ * @flow
+ */
+/* globals __DEV__ */
+import {
+  MeshBasicMaterial,
+  Texture,
+  ClampToEdgeWrapping,
+  NearestFilter,
+  Math
+} from 'three';
+import gifparser from "./gifparser";
 
+type Params = {
+  url: string,
+  autoplay: boolean
+};
 
-class GifMaterial {
+type Frame = {
+  disposalMethod: number,
+  blob: Blob,
+  width: number,
+  height: number
+};
 
+type GifData = {
+  delayTimes: any[],
+  loopCnt: number,
+  frames: Frame[],
+  timestamp: number,
+};
+
+class GifMaterial extends MeshBasicMaterial {
+  _cnv: HTMLCanvasElement;
+  _ctx: CanvasRenderingContext2D;
+  _texture: Texture;
+  _startTime: number = 0;
+  _endTime: number = 0;
+
+  _frames: any[] = [];
+  _frameCnt: number = 0;
+  _width: number = 0;
+  _height: number = 0;
+  _loopCnt: number = 0;
+  _infinity: boolean = false;
+  _nextFrameTime: number = 0;
+  _frameIdx: number = 0;
+  _delayTimes: number[] = [];
+
+  _isPaused: boolean = false;
+  _gifData: Object = {};
+
+  constructor(params: Params = {}) {
+    super();
+    // super props
+    this.name = 'GifMaterial';
+
+    // internal props
+    this._cnv = document.createElement('canvas');
+    this._cnv.width = 2;
+    this._cnv.height = 2;
+    this._ctx = this._cnv.getContext('2d');
+    this._texture = new Texture(this._cnv);
+    this._texture.wrapS = ClampToEdgeWrapping;
+    this._texture.wrapT = ClampToEdgeWrapping;
+    this._texture.minFilter = NearestFilter;
+    this.setValues({map: this._texture});
+    this._init(params);
+  }
+
+  static isGif = (buf: Uint8Array) => {
+    let p =0;
+    // - Header (GIF87a or GIF89a).
+    if (buf[p++] !== 0x47 ||            buf[p++] !== 0x49 || buf[p++] !== 0x46 ||
+      buf[p++] !== 0x38 || (buf[p++]+1 & 0xfd) !== 0x38 || buf[p++] !== 0x61) {
+      return false;
+    }
+  };
+
+  /**
+   * Initialize material
+   * if gif data is cached, then use it otherwise generate new gif data.
+   * then set the texture to start the animation loop.
+   * @param url
+   * @param autoplay
+   * @return {Promise<void>}
+   * @private
+   */
+  _init = async ({url, autoplay}: Params) => {
+    if(__DEV__) {
+      console.log('Initializing material');
+    }
+    try {
+      let gifData;
+      // check if we have already cached the data
+      if(this._gifData[url]) {
+        gifData = this._gifData[url];
+      } else {
+        gifData = await this._getGifData(url);
+      }
+      // override with what we have in cache
+      this._gifData[url] = gifData;
+      this. _setTexture(gifData);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  /**
+   * Make a xhr request to get the array buffer of the requested gif.
+   * Also validate if the buffer is a gif buffer
+   * @param url
+   * @return {Promise<*>}
+   * @private
+   */
+  _getUnit8Array = (url: string): Promise<Uint8Array | string> => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.responseType = 'arraybuffer';
+      xhr.addEventListener('load', (e: any) => {
+        const uint8Arr = new Uint8Array(e.target.response);
+        // extract header to test if this is a gif of not
+        if(!GifMaterial.isGif(uint8Arr)) {
+          reject('Not a gif');
+        }
+        resolve(uint8Arr);
+      });
+    });
+  };
+
+  /**
+   * parse the gif array buffer and generate img data for all frames
+   * @param url
+   * @return {Promise<void>}
+   * @private
+   */
+  _getGifData = (url: string): Promise<GifData> => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const uint8Array = await this._getUnit8Array(url);
+        const {delayTimes, loopCnt, frames} = await gifparser(uint8Array);
+        resolve({delayTimes, loopCnt, frames, timestamp: Date.now()});
+      } catch (e) {
+        reject(e);
+      }
+    });
+  };
+
+  _setTexture = ({delayTimes, loopCnt, frames}: GifData) => {
+    if(__DEV__) {
+      console.log('setting texture');
+    }
+    this._delayTimes = delayTimes;
+    loopCnt? (this._loopCnt = loopCnt): (this._infinity = true);
+    this._frames = frames;
+    this._frameCnt = delayTimes.length;
+    this._startTime = Date.now();
+    this._width = Math.floorPowerOfTwo(frames[0].width);
+    this._height = Math.floorPowerOfTwo(frames[0].height);
+    this._cnv.width = this._width;
+    this._cnv.height = this._height;
+
+    this._draw();
+    // todo start from herhehreh
+  };
+
+  _paused = () => this._isPaused;
+
+  _nextFrame = () => {
+    this._draw();
+
+    while((Date.now() - this._startTime) >= this._nextFrameTime) {
+      this._nextFrameTime += this._delayTimes[this._frameIdx++];
+    }
+  };
+
+  _draw = () => {
+    if(this._frameIdx !== 0) {
+      const lastFrame = this._frames[this._frameIdx - 1];
+      if(lastFrame.disposalMethod === 8 || lastFrame.disposalMethod === 9) {
+        // this._clearCanvas()
+      }
+    }
+  };
+
+  _clearCanvas = () => {
+    this._ctx.clearRect(0, 0, this._width, this._height);
+    this._texture.needsUpdate = true
+  };
+
+  frame(ms: number) {
+    if (!this._frames.length || this._paused()) {
+      return;
+    }
+    if (Date.now() - this._startTime >= this._nextFrameTime) {
+      this._nextFrame();
+    }
+  }
+
+  /**
+   * @override
+   */
+  dispose() {
+
+  }
 }
 
 export default GifMaterial;
---
+++ src/gifparser.js
@@ -0,0 +1,101 @@
+/**
+ *
+ * Gif parser by @gtk2k
+ * https://github.com/gtk2k/gtk2k.github.io/tree/master/animation_gif
+ *
+ */
+
+const gifparser = (gif) => {
+  return new Promise((resolve, reject) => {
+    let pos = 0;
+    let delayTimes = [];
+    let loadCnt = 0;
+    let graphicControl = null;
+    let imageData = null;
+    let frames = [];
+    let loopCnt = 0;
+    if (gif[0] === 0x47 && gif[1] === 0x49 && gif[2] === 0x46 && // 'GIF'
+      gif[3] === 0x38 && (gif[4] === 0x39 || gif[4] === 0x37) && gif[5] === 0x61) { // '89a'
+
+      pos += 13 + (+!!(gif[10] & 0x80) * Math.pow(2, (gif[10] & 0x07) + 1) * 3);
+      let gifHeader = gif.subarray(0, pos);
+      while (gif[pos] && gif[pos] !== 0x3b) {
+        let offset = pos, blockId = gif[pos];
+        if (blockId === 0x21) {
+          let label = gif[++pos];
+          if ([0x01, 0xfe, 0xf9, 0xff].indexOf(label) !== -1) {
+            label === 0xf9 && (delayTimes.push((gif[pos + 3] + (gif[pos + 4] << 8)) * 10));
+            label === 0xff && (loopCnt = gif[pos + 15] + (gif[pos + 16] << 8));
+            while (gif[++pos]) pos += gif[pos];
+            label === 0xf9 && (graphicControl = gif.subarray(offset, pos + 1));
+          } else {
+            reject('parseGIF: unknown label');
+            break;
+          }
+        } else if (blockId === 0x2c) {
+          pos += 9;
+          pos += 1 + (+!!(gif[pos] & 0x80) * (Math.pow(2, (gif[pos] & 0x07) + 1) * 3));
+          while (gif[++pos]) {
+            pos += gif[pos];
+          }
+          let imageData = gif.subarray(offset, pos + 1);
+          // Each frame should have an image and a flag to indicate how to dispose it.
+          let frame = {
+            // http://matthewflickinger.com/lab/whatsinagif/animation_and_transparency.asp
+            // Disposal method is a flag stored in the 3rd byte of the graphics control
+            // This byte is packed and stores more information, only 3 bits of it represent the disposal
+            disposalMethod: graphicControl[3],
+            blob: URL.createObjectURL(new Blob([gifHeader, graphicControl, imageData]))
+          };
+          frames.push(frame);
+        } else {
+          reject('parseGIF: unknown blockId');
+          break;
+        }
+        pos++;
+      }
+    } else {
+      reject('parseGIF: no GIF89a');
+    }
+    if (frames.length) {
+      let cnv = document.createElement('canvas');
+      let loadImg = function () {
+        for (let i = 0; i < frames.length; i++) {
+          let img = new Image();
+          img.onload = function (e, i) {
+            if (i === 0) {
+              cnv.width = img.width;
+              cnv.height = img.height;
+            }
+            loadCnt++;
+            frames[i] = this;
+            if (loadCnt === frames.length) {
+              loadCnt = 0;
+              imageFix(1);
+            }
+          }.bind(img, null, i);
+          // Link html image tag with the extracted GIF Frame
+          img.src = frames[i].blob;
+          img.disposalMethod = frames[i].disposalMethod;
+        }
+      };
+      let imageFix = function (i) {
+        let img = new Image();
+        img.onload = function (e, i) {
+          loadCnt++;
+          frames[i] = this;
+          if (loadCnt === frames.length) {
+            cnv = null;
+            resolve({delayTimes, loopCnt, frames});
+          } else {
+            imageFix(++i);
+          }
+        }.bind(img);
+        img.src = cnv.toDataURL('image/gif');
+      };
+      loadImg();
+    }
+  });
+};
+
+export default gifparser;
